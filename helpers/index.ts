a4f7ecc1a86702feb000d6f83ab3d1448954003d
Valmir Barbosa <suporte@valmirbarbosa.com.br>
2018-06-12 15:23:33 -0300

    helpers

---
+++ helpers/index.ts
@@ -0,0 +1,27 @@
+//
+// import { HttpParams } from '@angular/common/http';
+// import { FormGroup } from '@angular/forms';
+//
+//
+// export function toHttpParams2(obj: Object): HttpParams {
+//     return Object.getOwnPropertyNames(obj)
+//         .reduce((p: HttpParams, key) => p.set(key, obj[key]), new HttpParams());
+// }
+//
+// export function getAllErrors(form: FormGroup) {
+//     let hasError = false;
+//     const result = Object.keys(form.controls).reduce((acc, key) => {
+//         const control = form.get(key);
+//         const errors = control instanceof FormGroup
+//             ? this.getAllErrors(control)
+//             : control.errors;
+//         if (errors) {
+//             acc[key] = errors;
+//             hasError = true;
+//         }
+//         return acc;
+//     }, {} as { [key: string]: any; });
+//     console.log('has error' + form, result);
+//
+// }
+//
---
+++ responses/item-response.model.ts
@@ -0,0 +1,24 @@
+export interface ItemResponse {
+  status?: 'error' | 'success';
+  message?: string;
+  data: any;
+}
+
+export interface CollectionResponse {
+  data: any[];
+  includes?: any[];
+  pagination?: {};
+}
+
+export interface ErrorResponse {
+  status: string;
+  data?: any;
+  error?: Error;
+}
+
+interface Error {
+  code: string;
+  http_code: number;
+  message: string;
+  validation?: [{ string: string[] }];
+}
