ec6b9c652d107dd27fa2c7b19dbf51fe55d1f5b3
Valmir Barbosa <suporte@valmirbarbosa.com.br>
2018-12-27 03:31:40 -0200

    fix

--- services/notifications.service.ts
+++ services/notifications.service.ts
@@ -1,11 +1,10 @@
 import {Injectable} from '@angular/core';
 import {MatDialog, MatDialogRef, MatSnackBar} from '@angular/material';
+
 import {ToastrService} from 'ngx-toastr';
-import {RapiPiecesConfirmDialogComponent} from '../../piece/components/confirm-dialog/confirm-dialog.component';
-import {RequestDeleteWallet} from '../../../app/main/wallet/store/wallet.actions';
-import {toPromise} from 'rxjs-compat/operator/toPromise';
 import {Observable} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
+import {RapiPiecesConfirmDialogComponent} from '../../piece/components/confirm-dialog/confirm-dialog.component';
 
 @Injectable()
 export class W3NotificationService {
