597b86a32ad033c3f55c519eabe22ea2ee702d53
Valmir Barbosa <valmir.php@gmail.com>
2019-03-26 10:45:10 -0300

    Add annotations in helpers/value-accessor.ts

--- helpers/value-accessor.ts
+++ helpers/value-accessor.ts
@@ -1,37 +1,64 @@
 import {ControlValueAccessor} from '@angular/forms';
 
+/**
+ * Example:
+ *
+ @Component({
+    selector: 'je-md-input-view',
+    templateUrl: './je-md-input-view.component.html',
+    styleUrls: ['./je-md-input-view.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => JeMdInputViewComponent),
+            multi: true
+        }
+    ]
+ })
+ export class JeMdInputViewComponent extends ValueAccessorBase<string> implements OnInit {
+
+     demoUpdate():void{
+        this.value = "sdsdf";
+     }
+
+ }
+ */
 export abstract class ValueAccessorBase<T> implements ControlValueAccessor {
 
-  onChange = (value: T) => {
-  };
+    private innerValue: T;
 
-  onTouched = () => {
-  };
+    onChange = (value: T) => {
+    };
 
-  private innerValue: T;
+    onTouched = () => {
+    };
 
-  get value(): T {
-    return this.innerValue;
-  }
-
-  set value(value: T) {
-    if (this.innerValue !== value) {
-      this.innerValue = value;
-      // informação da view para o  controller
-      this.onChange(value);
+    get value(): T {
+        return this.innerValue;
     }
-  }
 
-  writeValue(value: T) {
-    // informação do controller para a view
-    this.innerValue = value;
-  }
+    set value(value: T) {
+        if (this.innerValue !== value) {
+            this.innerValue = value;
+            // informação da view para o  controller
+            this.onChange(value);
+            this.onTouched();
+        }
+    }
 
-  registerOnChange(fn: (_: any) => void): void {
-    this.onChange = fn;
-  }
+    /**
+     * responsavel por tratar as alterações feitas de fora
+     * @param value
+     */
+    writeValue(value: T): void {
+        this.innerValue = value;
+    }
 
-  registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
+    registerOnChange(fn: (_: any) => void): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: () => void): void {
+        this.onTouched = fn;
+    }
 }
