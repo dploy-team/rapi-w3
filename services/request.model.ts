411a56569782477cb04ffe7e09407bbcdb178571
Wagner Mattei <wagner.mattei@gmail.com>
2019-06-30 04:36:57 +0000

    Adicionando options no post da API

--- services/request.model.ts
+++ services/request.model.ts
@@ -1,6 +1,25 @@
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+
 export interface W3MetaPagination {
     page: number;
     total: number;
     per_page: number;
     current_page: number;
+}
+
+export interface HttpPostOptions {
+    headers?:
+        | HttpHeaders
+        | {
+              [header: string]: string | string[];
+          };
+    observe?: 'body';
+    params?:
+        | HttpParams
+        | {
+              [param: string]: string | string[];
+          };
+    reportProgress?: boolean;
+    responseType?: 'json';
+    withCredentials?: boolean;
 }
